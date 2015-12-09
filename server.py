847f9c1530f99fc1463e0cd8baa5d47188282595
G33kDude <ptubbytaylor@gmail.com>
2015-12-09 16:44:29 -0500

    Rewritten in python

---
+++ server.py
@@ -0,0 +1,41 @@
+import os
+import SimpleHTTPServer
+import time
+
+import ev3dev.core
+ev3dev.core.LegoPort.SYSTEM_CLASS_NAME = 'lego-port'
+
+SERVER_PORT = 8080
+DSWITCH_PORT = 'outD'
+
+class DSwitch:
+	def __init__(self, port):
+		ev3dev.core.LegoPort(DSWITCH_PORT).mode = 'led'
+		time.sleep(1)
+		self.led = ev3dev.core.Led(name=port+'::ev3dev')
+	def on(self):
+		self.led.brightness = self.led.max_brightness
+	def off(self):
+		self.led.brightness = 0
+
+class TreeV3Handler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+	def do_POST(self):
+		global dswitch
+		action = self.path.split('/')[-1]
+		if action == 'on':
+			dswitch.on()
+		elif action == 'off':
+			dswitch.off()
+		self.send_response(200)
+
+def treev3(handler_class, dswitch_port, server_path='.', server_port=8080,
+		server_class=SimpleHTTPServer.BaseHTTPServer.HTTPServer):
+	global dswitch
+	dswitch = DSwitch(dswitch_port)
+	os.chdir(server_path)
+	httpd = server_class(('', server_port), handler_class)
+	print("Running on port {}".format(SERVER_PORT))
+	httpd.serve_forever()
+
+if __name__ == '__main__':
+	treev3(TreeV3Handler, 'outD', './www')
